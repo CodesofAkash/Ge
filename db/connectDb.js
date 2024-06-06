765e723bc2d9e489d707cef448c47564e89c0ae2
Akash Sharma <akashcodesharma@gmail.com>
2024-06-06 14:38:15 +0530

    Final commit

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(`mongodb+srv://akashcodesharma:KillerAkay@getmeachai.5xf9gus.mongodb.net/?retryWrites=true&w=majority&appName=GetMeAChai`, {
+      const conn = await mongoose.connect(`mongodb+srv://akashcodesharma:KillerAkay@GetMeAChai.mongodb.net/GetMeAChai
+      `, {
         useNewUrlParser: true,
       });
       console.log(`MongoDB Connected: {conn.connection.host}`);
