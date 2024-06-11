d00476fec54d1398e2d4ce72f160a0b7709d41f7
Akash Sharma <akashcodesharma@gmail.com>
2024-06-11 17:16:40 +0530

    Done

--- db/connectDb.js
+++ db/connectDb.js
@@ -10,7 +10,7 @@ const connectDb = async () => {
   const uri = "mongodb+srv://akashcodesharma:KillerAkay@getmeachai.5xf9gus.mongodb.net/?retryWrites=true&w=majority&appName=GetMeAChai";
   const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
   
-  await mongoose.connect(uri, clientOptions);
+  await mongoose.connect(uri);
   console.log("Go")
   await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
