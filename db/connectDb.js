1a83bb5b8f4403fd04891d3aa86695769d3a210f
Akash Sharma <akashcodesharma@gmail.com>
2024-06-11 16:10:29 +0530

    Done

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,43 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-// const connectDB = async () => {
-//   const uri = "mongodb+srv://akashcodesharma:<password>@getmeachai.5xf9gus.mongodb.net/?retryWrites=true&w=majority&appName=GetMeAChai";
-//   const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-//       await mongoose.connect(uri, clientOptions);
-//       await mongoose.connection.db.admin().command({ ping: 1 });
-//       console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//       // const conn = await mongoose.connect("mongodb+srv://akashcodesharma:KillerAkay@getmeachai.5xf9gus.mongodb.net/?retryWrites=true&w=majority&appName=GetMeAChai");
-//       // console.log(`MongoDB Connected: ${conn.connection.host}`);
-    
-//   }
+const connectDb = async () => {
+  if (mongoose.connection.readyState >= 1) return;
 
-//   export default connectDB;
+  return mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
 
-// // async function run() {
-// //   try {
-// //     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-// //   } finally {
-// //     // Ensures that the client will close when you finish/error
-// //     await mongoose.disconnect();
-// //   }
-// // }
-// // run().catch(console.dir);
-
-
-// const mongoose = require('mongoose');
-const uri = "mongodb+srv://akashcodesharma:KillerAkay@getmeachai.5xf9gus.mongodb.net/?retryWrites=true&w=majority&appName=GetMeAChai";
-
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-
-async function run() {
-  try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
-  }
-}
-run().catch(console.dir);
+export default connectDb;
