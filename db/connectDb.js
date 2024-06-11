ce944fdbce2d1319f99ff068cf88339d3d5f913d
Akash Sharma <akashcodesharma@gmail.com>
2024-06-11 17:04:02 +0530

    Done

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,9 +2,12 @@
 import mongoose from 'mongoose';
 
 const connectDb = async () => {
+  console.log("Ready")
   if (mongoose.connection.readyState >= 1) return;
+  console.log("Steady")
 
   return mongoose.connect(process.env.MONGODB_URI);
+  console.log("Go")
 };
 
 export default connectDb;
