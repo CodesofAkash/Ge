b640a55f9364df78a4b410cc4e4b911dfe897c3a
Akash Sharma <akashcodesharma@gmail.com>
2024-06-11 17:29:52 +0530

    Done

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,24 +1,16 @@
 import mongoose from 'mongoose';
 
-// Connection options
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // Other options such as poolSize can be added here
-};
-
 // Initialize MongoDB connection
 const connectDb = async () => {
   try {
     if (mongoose.connection.readyState !== 1) {
-      await mongoose.connect(process.env.MONGODB_URI, options);
+      await mongoose.connect(process.env.MONGODB_URI);
       console.log("Connected to MongoDB");
     } else {
       console.log("Already connected to MongoDB");
     }
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    // You can choose to handle the error here, retry connection, or throw it
     throw error;
   }
 };
