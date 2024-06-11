e9c3e2c5468ade7ab7f43a067a0b8ff93a8e20a6
Akash Sharma <akashcodesharma@gmail.com>
2024-06-11 14:16:42 +0530

    Final Commit

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,24 +1,43 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  const uri = "mongodb+srv://akashcodesharma:<password>@getmeachai.5xf9gus.mongodb.net/?retryWrites=true&w=majority&appName=GetMeAChai";
-  const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-      await mongoose.connect(uri, clientOptions);
-      await mongoose.connection.db.admin().command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-      // const conn = await mongoose.connect("mongodb+srv://akashcodesharma:KillerAkay@getmeachai.5xf9gus.mongodb.net/?retryWrites=true&w=majority&appName=GetMeAChai");
-      // console.log(`MongoDB Connected: ${conn.connection.host}`);
+// const connectDB = async () => {
+//   const uri = "mongodb+srv://akashcodesharma:<password>@getmeachai.5xf9gus.mongodb.net/?retryWrites=true&w=majority&appName=GetMeAChai";
+//   const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+//       await mongoose.connect(uri, clientOptions);
+//       await mongoose.connection.db.admin().command({ ping: 1 });
+//       console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//       // const conn = await mongoose.connect("mongodb+srv://akashcodesharma:KillerAkay@getmeachai.5xf9gus.mongodb.net/?retryWrites=true&w=majority&appName=GetMeAChai");
+//       // console.log(`MongoDB Connected: ${conn.connection.host}`);
     
-  }
-
-  export default connectDB;
-
-// async function run() {
-//   try {
-//     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await mongoose.disconnect();
 //   }
-// }
-// run().catch(console.dir);
+
+//   export default connectDB;
+
+// // async function run() {
+// //   try {
+// //     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+// //   } finally {
+// //     // Ensures that the client will close when you finish/error
+// //     await mongoose.disconnect();
+// //   }
+// // }
+// // run().catch(console.dir);
+
+
+// const mongoose = require('mongoose');
+const uri = "mongodb+srv://akashcodesharma:KillerAkay@getmeachai.5xf9gus.mongodb.net/?retryWrites=true&w=majority&appName=GetMeAChai";
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
