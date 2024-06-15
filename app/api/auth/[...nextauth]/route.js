f91b8cdde3e256363fad00539ed2e1d620a327e0
Akash Sharma <akashcodesharma@gmail.com>
2024-06-15 07:23:50 +0530

    Done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -42,16 +42,16 @@ export const authoptions = NextAuth({
             if (account.provider === "google") {
                 console.log("\nHello Hi Bye\n");
                 await connectDB();
-                // const currentUser = await User.findOne({ email: user.email});
-                // if(!currentUser) {
-                //     const newUser = await User.create({
-                //         email: user.email,
-                //         username: profile.name,
-                //         name: user.name,
-                //         provider: account.provider,
-                //     })
+                const currentUser = await User.findOne({ email: user.email});
+                if(!currentUser) {
+                    const newUser = await User.create({
+                        email: user.email,
+                        username: profile.name,
+                        name: user.name,
+                        provider: account.provider,
+                    })
                 console.log("\nHello Hi Byeeee\n");
-                // }
+                }
             }
             return true;
         },
